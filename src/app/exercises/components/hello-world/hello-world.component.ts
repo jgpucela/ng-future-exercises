0b4c811587149695f0067c394d904c08382c0650
Jorge <jgpucela@users.noreply.github.com>
2024-04-11 10:23:12 +0000

    primer componente

--- src/app/exercises/components/hello-world/hello-world.component.ts
+++ src/app/exercises/components/hello-world/hello-world.component.ts
@@ -3,6 +3,14 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  template: ``,
+  template: `<h1 class="title">
+    Hello World!
+    <h1></h1>
+  </h1>`,
+  styles: `
+    .title {
+      color: var(--primary-color);
+    }
+  `,
 })
 export class HelloWorldComponent {}
--- src/app/exercises/typescript/generics/exercise/generics.ts
+++ src/app/exercises/typescript/generics/exercise/generics.ts
@@ -1,1 +1,11 @@
 // @ts-nocheck
+function identity<Type>(arg: Type): Type {
+  return arg
+}
+
+class Generic<ReturnType, Type = string> {
+  foo?: Type
+  bar(x: Type): ReturnType {
+    return x as ReturnType
+  }
+}
